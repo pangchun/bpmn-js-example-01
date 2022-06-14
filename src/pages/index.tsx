28f807e44650d2725ded6e9c6d651258a3ff6b7f
pangchun <pangchun_mail@qq.com>
2022-06-14 23:25:28 +0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import ProgressDesigner from '@/components/PregressDesigner/ProgressDesigner';
+import styles from './index.less';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <ProgressDesigner />
+    </div>
+  );
+}
---
+++ typings.d.ts
@@ -0,0 +1,13 @@
+declare module '*.css';
+declare module '*.less';
+declare module '*.png';
+declare module '*.svg' {
+  export function ReactComponent(
+    props: React.SVGProps<SVGSVGElement>,
+  ): React.ReactElement;
+  const url: string;
+  export default url;
+}
+
+// tsx 默认不引入js，设置忽略类型
+declare module '*';
